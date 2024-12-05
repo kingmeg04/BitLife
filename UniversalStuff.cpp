e295572c71af6ddc5e1840746977b18f6a8f06c4
kingmeg04 <kingmag1804@outlook.com>
2024-12-05 12:34:26 +0100

    Committed

--- UniversalStuff.cpp
+++ UniversalStuff.cpp
@@ -1,3 +1,11 @@
-//
-// Created by jbrui on 05.12.2024.
-//
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+double random(double min, double max) {
+    static random_device rd;  // Seed
+    static mt19937 gen(rd()); // Random number generator
+    uniform_real_distribution<> dis(min, max);
+    return dis(gen);
+}
