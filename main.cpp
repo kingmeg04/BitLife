38ee0b07d99310bf5405cf17426da6ca81a03211
kingmeg04 <kingmag1804@outlook.com>
2024-12-05 13:33:10 +0100

    Committed

--- main.cpp
+++ main.cpp
@@ -8,14 +8,14 @@ using namespace std;
 
 int main() {
 
-
     Crime pickpocket = {100, 0.05, "pickpocketing"};
     Crime shoplift = {200, 0.2, "shoplifting"};
     Crime assault = {500, 0.9, "assault"};
     Crime rape = {750, 0.95, "rape"};
-    Crime murder = {900, 0.5, "murder"};
-    Crime explosiveDiarrheaInPublicSpace = {50, 0.99, "explosive diarrhea in public space"};
+    Crime murder = {900, 0.5, "murder", 10};
+    Crime explosiveDiarrheaInPublicSpace = {50, 0.99, "explosive diarrhea in public space", 1000};
 
+    //caught for a crime (here: murder)
     array<int, 2> x = prisonTime({pickpocket, shoplift, assault, rape, murder, explosiveDiarrheaInPublicSpace}, murder);
 
     pickpocket.sTimesCommitted = 0;
@@ -24,9 +24,9 @@ int main() {
     rape.sTimesCommitted = 0;
     murder.sTimesCommitted = 0;
     explosiveDiarrheaInPublicSpace.sTimesCommitted = 0;
+    //end of being caught for a crime
 
-    //array<Crime, 6> BadBoyArray = {pickpocket, shoplift, assault, rape, murder, explosiveDiarrheaInPublicSpace};
-    //Crime BadBoyArray[] = {pickpocket, shoplift, assault, rape, murder, explosiveDiarrheaInPublicSpace};
+
 
 
 
