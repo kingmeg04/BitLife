fa486c795e0fceaa2fa6a0164ba4e7c60ab2b7c2
kingmeg04 <kingmag1804@outlook.com>
2024-12-06 07:53:01 +0100

    Committed

--- main.cpp
+++ main.cpp
@@ -10,20 +10,20 @@ using namespace std;
 int main() {
     SetConsoleOutputCP( 65001 );
     //player stuff
-    int totalMoney; //the players amount of money
+    int totalMoney = 0; //the players amount of money
 
     //criminal stuff
     int paidFees; //temporary value for paying off crime fees
 
-    Crime pickpocket = {100, 0.05, "pickpocketing", 100};
+    Crime pickpocket = {100, 0.05, "pickpocketing"};
     Crime shoplift = {200, 0.2, "shoplifting"};
     Crime assault = {500, 0.9, "assault"};
     Crime rape = {750, 0.95, "rape"};
     Crime murder = {900, 0.5, "murder"};
     Crime explosiveDiarrheaInPublicSpace = {50, 0.99, "explosive diarrhea in public space"};
 
-    //caught for a crime (here: murder)
-    array<int, 2> prisonCharges = prisonCharge({pickpocket, shoplift, assault, rape, murder, explosiveDiarrheaInPublicSpace}, murder);
+    //caught for a crime (here: assault)
+    array<int, 2> prisonCharges = prisonCharge({pickpocket, shoplift, assault, rape, murder, explosiveDiarrheaInPublicSpace}, assault);
 
     pickpocket.sTimesCommitted = 0;
     shoplift.sTimesCommitted = 0;
@@ -45,8 +45,21 @@ int main() {
             prisonCharges[0] -= paidFees;
         }
 
-        prisonCharges[1] = static_cast<int>(floor(prisonCharges[0] / 100));
+        prisonCharges[1] += static_cast<int>(floor(prisonCharges[0] / 100));
         totalMoney -= paidFees;
+        cout << totalMoney << endl;
+        if(prisonCharges[1] > 0) {
+            cout << "You paid " << paidFees << "$ and will spend " << prisonCharges[1] << " days in Prison";
+            if(prisonCharges[1] > 365) {
+                cout << " (" << round(prisonCharges[1]/365*4) / 4 << " years)" << endl;
+            }
+            else {
+                cout << endl;
+            }
+        }
+        else {
+            cout << "You paid " << paidFees << "$ to avoid jail time" << endl;
+        }
     }
     //end of being caught for a crime
 
