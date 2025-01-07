58df414fadb2e818b230ecc77f4aaac0f5513333
kingmeg04 <kingmag1804@outlook.com>
2025-01-07 16:01:18 +0100

    added criminalReputation variable for crime and playerActionPoints

--- PlayerStuff/PlayerStuff.h
+++ PlayerStuff/PlayerStuff.h
@@ -16,9 +16,12 @@ class player {
 
 public:
 
+
     job jCurrentJob;
     long balance = 0;
     std::vector<std::pair<item, int>> vItems;
+
+    short playerActionPoints = 5; // how many actions the player can perform per day
     short saturation = 100;
     short mentalHealth = 100;
     short criminalReputation = 0; //How involved in the crime world you are
