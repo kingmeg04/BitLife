a01a10c2ef60371d1cdc23ca9894dbdb84d4b390
kingmeg04 <kingmag1804@outlook.com>
2025-01-07 15:48:55 +0100

    added criminalReputation variable for crime

--- PlayerStuff/PlayerStuff.h
+++ PlayerStuff/PlayerStuff.h
@@ -21,6 +21,7 @@ public:
     std::vector<std::pair<item, int>> vItems;
     short saturation = 100;
     short mentalHealth = 100;
+    short criminalReputation = 0;
 
     player(job CurrentJob) : jCurrentJob(CurrentJob) {}
 
